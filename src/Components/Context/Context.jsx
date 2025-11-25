ab260dc03c8164b3332a547665a59bf30a9a6888
Adriano Braga <adrianobragamathias007@gmail.com>
2025-11-25 11:38:47 +0000

    Const criada para pegar informações da API

--- src/Components/Context/Context.jsx
+++ src/Components/Context/Context.jsx
@@ -3,9 +3,10 @@ import { createContext, useContext,useState } from "react";
 export const Context = createContext();
 
 export const ContextProvider = ({children}) => {
-
- const valores = {
-
+    const [Buscar,setBuscar] = useState([]);
+    const valores = {
+    Buscar:Buscar,
+    setBuscar:setBuscar, 
  }
 
     return(
