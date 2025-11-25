acc7c62f21bd09d58e2167ff7a705fde9a087a42
Adriano Braga <adrianobragamathias007@gmail.com>
2025-11-25 14:56:29 +0000

    Informações dos países adicionados tudo em paragrafo <p>

--- src/Components/Paises/Paises.jsx
+++ src/Components/Paises/Paises.jsx
@@ -1,8 +1,10 @@
 import { useContext } from "react";
 import { Context } from "../Context/Context";
+
 function Paises() {
-    const {Buscar} = useContext(Context);
-    console.log(Buscar)
+    const { Buscar } = useContext(Context);
+    console.log(Buscar);
+
     return (
         <div className="p-10">
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
@@ -15,8 +17,14 @@ function Paises() {
                             <img src={pais.flags.png} className="w-full h-40" />
                         </div>
 
-                        <h2 className="text-xl font-semibold">{pais.nome}</h2>
-                        <p className="text-sm text-slate-300">Continente: {pais.continente}</p>
+                        <h2 className="text-xl font-semibold">{pais.name.common}</h2>
+                        <p className="text-sm text-slate-300">Continente: {pais.region}</p>
+                        <p className="text-sm text-slate-300">Fronteira:{pais.borders && pais.borders.length > 0 ? pais.borders.join(", ") : "Nenhuma"}</p>
+                        <p className="text-sm text-slate-300">Capital: {pais.capital}</p>
+                        <p className="text-sm text-slate-300">Línguas: {pais.languages ? Object.values(pais.languages).join(", ") : "Não informado"}</p>
+                        <p className="text-sm text-slate-300">População: {pais.population}</p>
+                        <p className="text-sm text-slate-300">Moeda: {pais.currencies ? Object.values(pais.currencies).map(c => `${c.name} (${c.symbol})`).join(", "): "Não informado"}</p>
+                        
                     </div>
                 ))}
             </div>
