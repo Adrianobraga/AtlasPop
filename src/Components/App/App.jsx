1547986782ec9790b57531dde0d182a1c14b54da
Adriano Braga <adrianobragamathias007@gmail.com>
2025-11-25 11:37:57 +0000

    API foi colocado no App

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,8 +1,10 @@
 import Paises from "../Paises/Paises"
 import Pesquisa from "../Pesquisa/Pesquisa"
+import Api from "../API/Api"
 function App() {    
   return (
     <>
+    <Api />
     <Pesquisa />
     <Paises />
     </>
