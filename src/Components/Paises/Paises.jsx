a646c5211e0faecf16eb7b58f872d2ab7ba5076c
Adriano Braga <adrianobragamathias007@gmail.com>
2025-11-25 11:39:51 +0000

    por enquanto essa parte esta aparecendo somente as imagens da bandeira

--- src/Components/Paises/Paises.jsx
+++ src/Components/Paises/Paises.jsx
@@ -1,23 +1,18 @@
+import { useContext } from "react";
+import { Context } from "../Context/Context";
 function Paises() {
-    const paises = [
-        { bandeira: "🇧🇷", nome: "Brasil", continente: "América do Sul" },
-        { bandeira: "🇯🇵", nome: "Japão", continente: "Ásia" },
-        { bandeira: "🇫🇷", nome: "França", continente: "Europa" },
-        { bandeira: "🇨🇦", nome: "Canadá", continente: "América do Norte" },
-        { bandeira: "🇩🇪", nome: "Alemanha", continente: "Europa" },
-        { bandeira: "🇦🇺", nome: "Austrália", continente: "Oceania" },
-    ];
-
+    const {Buscar} = useContext(Context);
+    console.log(Buscar)
     return (
         <div className="p-10">
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-                {paises.map((pais, index) => (
+                {Buscar.map((pais, index) => (
                     <div
                         key={index}
                         className="bg-slate-800 text-white p-4 rounded-xl shadow-xl hover:scale-105 duration-300 cursor-pointer w-full"
                     >
-                        <div className="w-full h-32 bg-slate-600 rounded-lg mb-3 flex items-center justify-center">
-                            <span className="text-4xl">{pais.bandeira}</span>
+                        <div className="w-full h-40 bg-slate-600 rounded-lg mb-3 flex items-center justify-center">
+                            <img src={pais.flags.png} className="w-full h-40" />
                         </div>
 
                         <h2 className="text-xl font-semibold">{pais.nome}</h2>
