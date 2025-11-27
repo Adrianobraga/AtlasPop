b49767c1320b28e14fdca89a32ce1464e55f3243
Adriano Braga <adrianobragamathias007@gmail.com>
2025-11-27 18:49:37 +0000

    imagem posicionada no canto direito inferior para mudar a pagina de favoritos

---
+++ src/Components/Favorito/Favorito.jsx
@@ -0,0 +1,9 @@
+function Favorito () {
+    return(
+    <>
+    <div className="h-20 w-20 p-5 bg-gray-400 hover:bg-gray-500 fixed bottom-10 right-10 rounded-full"><img className="h-10" src="HeartLike.png" alt="" /></div>
+    </>
+    )
+}
+
+export default Favorito;
