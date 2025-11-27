638f53634e2445ea47ca698703de2bdaf9099ced
Adriano Braga <adrianobragamathias007@gmail.com>
2025-11-27 18:46:21 +0000

    Botão de favorito adicionado sem função ele so foi colocado no lugar

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,5 +1,6 @@
 import Paises from "../Paises/Paises"
 import Pesquisa from "../Pesquisa/Pesquisa"
+import Favorito from "../Favorito/Favorito"
 import Api from "../API/Api"
 function App() {    
   return (
@@ -7,6 +8,7 @@ function App() {
     <Api />
     <Pesquisa />
     <Paises />
+    <Favorito />
     </>
   )
 }
