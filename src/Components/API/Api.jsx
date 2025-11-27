f9f879ae02968d1b010af0a30430dc57d6353fb1
Adriano Braga <adrianobragamathias007@gmail.com>
2025-11-27 15:37:49 +0000

    if else para pesquisar o pais de acordo com a API

--- src/Components/API/Api.jsx
+++ src/Components/API/Api.jsx
@@ -2,22 +2,34 @@ import axios from "axios";
 import { useContext, useEffect } from "react";
 import { Context } from "../Context/Context";
 
-function Api () {
-    const {setBuscar,Filter} = useContext(Context);
-     useEffect(() => {
-    const Buscar = (filtrar) => {
-    axios
-    .get(`https://restcountries.com/v3.1/${filtrar ? filtrar : "all"}?fields=name,flags,capital,region,population,languages,currencies,borders`)
-    .then((res) => {
-        setBuscar(res.data)
-    })
-    .catch((error) => {
-        console.log(error)
-    })
-    }
-        Buscar(Filter);
-  }, [Filter,setBuscar]);
+function Api() {
+  const { setBuscar, Filter, Nome } = useContext(Context);
 
+  useEffect(() => {
+    const Buscar = (filtrar) => {
+      axios
+        .get(
+          `https://restcountries.com/v3.1/${filtrar ? filtrar : "all"}?fields=name,flags,capital,region,population,languages,currencies,borders`
+        )
+        .then((res) => {
+          if (Nome) {
+            const Pais = res.data.find(
+              (country) =>
+                country.name.common.toLowerCase() === Nome.toLowerCase()
+            );
+            setBuscar(Pais ? [Pais] : []);
+          } else {
+            setBuscar(res.data);
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+          setBuscar([]);
+        });
+    };
+
+    Buscar(Filter);
+  }, [Filter, Nome, setBuscar]);
 }
 
 export default Api;
