156623b37e825ec960aaaad3fe9e913e74992c64
Adriano Braga <adrianobragamathias007@gmail.com>
2025-12-10 10:40:11 +0000

    Foi colocado uma função para limpar nome e filtro

--- src/Components/Favorito/FavoritoBotão.jsx
+++ src/Components/Favorito/FavoritoBotão.jsx
@@ -1,8 +1,13 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { Context } from "../Context/Context";
 function Favoritobotao () {
+ const { Serasa } = useContext(Context);
+
+  
     return(
     <>
-  <Link to="/Favoritos">  <div className="h-20 w-20 p-5 bg-gray-400 hover:bg-gray-500 fixed bottom-10 right-10 rounded-full"><img className="h-10" src="HeartLike.png" alt="" /></div> </Link>
+  <Link to="/Favoritos" onClick={Serasa}>  <div className="h-20 w-20 p-5 bg-gray-400 hover:bg-gray-500 fixed bottom-10 right-10 rounded-full" ><img className="h-10" src="HeartLike.png" alt="" /></div> </Link>
     </>
     )
 }
