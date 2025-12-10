31711cb2cfce6a05036ff698b51b725a7e26ec7c
Adriano Braga <adrianobragamathias007@gmail.com>
2025-12-10 10:08:56 +0000

    Botão para voltar para tela inicial

---
+++ src/Components/Favorito/Voltar/Voltar.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+function Voltar () {
+    return(
+    <>
+  <Link to="/">  <div className="h-10 w-10 py-2 px-4 bg-gray-400 hover:bg-gray-500 fixed top-10 left-10 rounded-full"> X </div> </Link>
+    </>
+    )
+}
+
+export default Voltar;
